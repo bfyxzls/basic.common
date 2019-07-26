1c4e63a13eb94dfb33fc8baf4c7e77358a7f1203
zhangzhanling <zhangzl@i-counting.cn>
2019-07-26 18:24:46 +0800

    dto基本类和日志委托

--- src/main/java/com/lind/basic/common/dto/BaseDto.java
+++ src/main/java/com/lind/basic/common/dto/BaseDto.java
@@ -1,5 +1,6 @@
 package com.lind.basic.common.dto;
 
+import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 
@@ -7,7 +8,28 @@ import org.springframework.beans.BeanUtils;
  * 数据传递对象基本类.
  */
 @Slf4j
-public class BaseDto {
+public abstract class BaseDto {
+  /**
+   * 将dto类型转换为一个entity类型.
+   *
+   * @param clazz entity类型
+   * @return
+   */
+  public <P> P toMapper(Class<P> clazz, Consumer<String> logger) {
+    P p = null;
+    try {
+      p = clazz.newInstance();
+    } catch (InstantiationException | IllegalAccessException e) {
+      if (logger == null) {
+        log.error("Param NewInstance Error");
+      } else {
+        logger.accept("Param NewInstance Error");
+      }
+    }
+    BeanUtils.copyProperties(this, p);
+    return p;
+  }
+
   /**
    * 将dto类型转换为一个entity类型.
    *
@@ -15,13 +37,6 @@ public class BaseDto {
    * @return
    */
   public <P> P toMapper(Class<P> clazz) {
-    P p = null;
-    try {
-      p = clazz.newInstance();
-    } catch (InstantiationException | IllegalAccessException e) {
-      log.error("Param NewInstance Error");
-    }
-    BeanUtils.copyProperties(this, p);
-    return p;
+    return toMapper(clazz, null);
   }
 }
