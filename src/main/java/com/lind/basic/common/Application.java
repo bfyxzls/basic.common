0a655e70b81af89cb54e5761e160d85da845db40
zhangzhanling <zhangzl@i-counting.cn>
2019-09-02 10:42:49 +0800

    添加了docker支持

--- src/main/java/com/lind/basic/common/Application.java
+++ src/main/java/com/lind/basic/common/Application.java
@@ -2,12 +2,17 @@ package com.lind.basic.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
+  public String info() {
+    return "basic.common.info";
+  }
 }
